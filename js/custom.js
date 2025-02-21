befab8cada34b0dd4dfbfe670538b82e2cd97826
desipossa <c1jang@hanmail.net>
2025-02-21 13:56:23 +0900

    20250221

--- js/custom.js
+++ js/custom.js
@@ -25,3 +25,31 @@ $(function () {
     const MNS2 = new Swiper('.main_news_slide2');
 });
 
+//반응형 
+$(function () {
+    $('.header .mbtn').on('click', function () {
+        $('.header .gnb').toggleClass('on')
+    });
+
+
+    $('.header .gnb>ul>li>a').on('click', function (e) {
+        if ($(this).next().is('ul') && $('.header .gnb').hasClass('on')) {
+            e.preventDefault();
+            $('.header .gnb>ul>li ul').stop().slideUp();
+            $(this).next().stop().slideDown();
+        }
+
+    });
+
+
+    $(window).on('resize', function () {
+        let w = $(window).width();
+
+        if (w > 768) {
+            $('.header .gnb').removeClass('on');
+            $('.header .gnb>ul>li ul').removeAttr('style');
+        }
+
+    })
+})
+
